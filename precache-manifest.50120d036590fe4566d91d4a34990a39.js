1c5f6dcbb40f4d3f1043b54988dc209077d36eb7
Hoang Nguyen <hnguyen0428@yahoo.com>
2019-08-14 20:56:58 -0700

    Updates

---
+++ precache-manifest.50120d036590fe4566d91d4a34990a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a8a7e6726414d0f91395dac807d73e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a331b84fdda0d2e1a929",
+    "url": "/portfolio/static/css/main.5c332e48.chunk.css"
+  },
+  {
+    "revision": "c7885f39c42a3f2bb6a7",
+    "url": "/portfolio/static/js/2.a9a1ff4e.chunk.js"
+  },
+  {
+    "revision": "a331b84fdda0d2e1a929",
+    "url": "/portfolio/static/js/main.c3308c13.chunk.js"
+  },
+  {
+    "revision": "9fcfbba918bc9c01dbe4",
+    "url": "/portfolio/static/js/runtime~main.5add31fb.js"
+  }
+]);
