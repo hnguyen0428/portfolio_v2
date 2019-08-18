2abc5d9fc48370f040f215e81877858a05987903
Hoang Nguyen <hnguyen0428@yahoo.com>
2019-08-17 17:12:52 -0700

    Updates

---
+++ precache-manifest.cc2277e52142612582af19cb70752642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275123c234850cd577c65b4e1561f93c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b677a0a4d6457ff10f4",
+    "url": "/portfolio/static/css/main.5a4cdb9f.chunk.css"
+  },
+  {
+    "revision": "1ae0ac31cf223a352994",
+    "url": "/portfolio/static/js/2.319d9181.chunk.js"
+  },
+  {
+    "revision": "9b677a0a4d6457ff10f4",
+    "url": "/portfolio/static/js/main.a4007232.chunk.js"
+  },
+  {
+    "revision": "9fcfbba918bc9c01dbe4",
+    "url": "/portfolio/static/js/runtime~main.5add31fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.aca47970c4fd3879f2ae4ab27d3ef6cf.js"
+  "/portfolio/precache-manifest.cc2277e52142612582af19cb70752642.js"
 );
 
 self.addEventListener('message', (event) => {
